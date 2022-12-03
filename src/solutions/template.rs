80df95de9d32549771d5bb2dee8f01365efffe4b
JasonCubed132 <amcb2002@googlemail.com>
2022-12-03 23:42:53 +0000

    Update template

--- src/solutions/template.rs
+++ src/solutions/template.rs
@@ -30,10 +30,8 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("[1,2,3]", 6, 18)]
-    #[case("[0,7,13,20,1,100]", 141, 846)]
-    #[case("[6000]", 6000, 6000)]
-    fn validate_linear(#[case] input: &str, #[case] expected_1: usize, #[case] expected_2: usize) {
+    #[case("", 0, 0)]
+    fn validate_linear(#[case] input: &str, #[case] expected_1: _, #[case] expected_2: _) {
         let mut input = Day2Solution::load(input).unwrap();
         let p1 = Day2Solution::part1(&mut input).unwrap();
         let p2 = Day2Solution::part2(&mut input, p1).unwrap();
