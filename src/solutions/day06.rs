b2f5341c38ac03c1572f32fe479d5eb186d0c9e6
JasonCubed132 <amcb2002@googlemail.com>
2022-12-06 13:43:39 +0000

    Fun with sets

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,4 +1,6 @@
 
+use std::collections::HashSet;
+
 use crate::utils::solver_types::{solve_linear, SolutionLinear};
 use anyhow::{anyhow, Result};
 use itertools::Itertools;
@@ -11,10 +13,26 @@ pub fn day06(input: &str) -> Result<f32> {
 
 impl SolutionLinear<Vec<char>, i32, i32> for Day6Solution {
     fn load(input: &str) -> Result<Vec<char>> {
-        todo!()
+        let lines = input.lines().collect_vec();
+        if lines.len() != 1 {
+            return Err(anyhow!("Expected 1 line"));
+        }
+
+        let output = lines[0].chars().collect_vec();
+        if output.len() < 4 {
+            return Err(anyhow!("Not enough elements"));
+        }
+
+        Ok(output)
     }
 
     fn part1(input: &mut Vec<char>) -> Result<i32> {
+        let mut set: HashSet<char> = HashSet::new();
+
+        let mut start_index = 0;
+        for i in 0..3 {
+            set.insert(input[i]);
+        }
         todo!()
     }
 
@@ -30,7 +48,11 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("", 0, 0)]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 0)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 0)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 6, 0)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 0)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 0)]
     fn validate_linear(#[case] input: &str, #[case] expected_1: i32, #[case] expected_2: i32) {
         let mut input = Day6Solution::load(input).unwrap();
         let p1 = Day6Solution::part1(&mut input).unwrap();
