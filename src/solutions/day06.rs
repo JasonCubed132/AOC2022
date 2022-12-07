bc0486fdd451bbd406e21edaa8f722576ec36b73
JasonCubed132 <amcb2002@googlemail.com>
2022-12-07 19:39:41 +0000

    Day 6 finished

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -11,6 +11,18 @@ pub fn day06(input: &str) -> Result<f32> {
     solve_linear::<Day6Solution, _, _, _>(input)
 }
 
+fn generic_solution(input: &mut Vec<char>, len_of_set: usize) -> Result<i32> {
+    for index in 0..(input.len() - len_of_set) {
+        let set: HashSet<char> = input[index..index+len_of_set].into_iter().copied().collect();
+        if set.len() == len_of_set {
+            let result: i32 = (index + len_of_set).try_into().unwrap();
+            return Ok(result)
+        }
+    }
+
+    Err(anyhow!("Unique set not found"))
+}
+
 impl SolutionLinear<Vec<char>, i32, i32> for Day6Solution {
     fn load(input: &str) -> Result<Vec<char>> {
         let lines = input.lines().collect_vec();
@@ -27,17 +39,11 @@ impl SolutionLinear<Vec<char>, i32, i32> for Day6Solution {
     }
 
     fn part1(input: &mut Vec<char>) -> Result<i32> {
-        let mut set: HashSet<char> = HashSet::new();
-
-        let mut start_index = 0;
-        for i in 0..3 {
-            set.insert(input[i]);
-        }
-        todo!()
+        generic_solution(input, 4)
     }
 
-    fn part2(input: &mut Vec<char>, part_1_solution: i32) -> Result<i32> {
-        todo!()
+    fn part2(input: &mut Vec<char>, _part_1_solution: i32) -> Result<i32> {
+        generic_solution(input, 14)
     }
 }
 
@@ -48,11 +54,11 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 0)]
-    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 0)]
-    #[case("nppdvjthqldpwncqszvftbrmjlhg", 6, 0)]
-    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 0)]
-    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 0)]
+    #[case("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19)]
+    #[case("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23)]
+    #[case("nppdvjthqldpwncqszvftbrmjlhg", 6, 23)]
+    #[case("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29)]
+    #[case("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26)]
     fn validate_linear(#[case] input: &str, #[case] expected_1: i32, #[case] expected_2: i32) {
         let mut input = Day6Solution::load(input).unwrap();
         let p1 = Day6Solution::part1(&mut input).unwrap();
