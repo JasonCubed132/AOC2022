ef577b372d964b220c7d8ce61039bb8dcbf5c089
JasonCubed132 <amcb2002@googlemail.com>
2022-12-16 16:19:59 +0000

    Day 8 p1

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -11,6 +11,7 @@ pub fn day08(input: &str) -> Result<f32> {
 impl SolutionLinear<Vec<Vec<i32>>, i32, i32> for Day8Solution {
     fn load(input: &str) -> Result<Vec<Vec<i32>>> {
         //https://stackoverflow.com/questions/43983414/how-to-convert-a-rust-char-to-an-integer-so-that-1-becomes-1#43985962
+        //https://stackoverflow.com/questions/67829405/how-to-convert-a-u32-constant-to-i32-constant-without-unsafe-in-rust
         let forest = input
             .lines()
             .map(|line| {
@@ -23,7 +24,99 @@ impl SolutionLinear<Vec<Vec<i32>>, i32, i32> for Day8Solution {
     }
 
     fn part1(input: &mut Vec<Vec<i32>>) -> Result<i32> {
-        todo!()
+        let height = input.len();
+        let width = input[0].len();
+
+        let mut visible_map = vec![vec![0; width]; height];
+
+        // Fill initial
+        for i in 0..width {
+            visible_map[0][i] = 1;
+            visible_map[height - 1][i] = 1;
+        }
+
+        for i in 1..height - 1 {
+            visible_map[i][0] = 1;
+            visible_map[i][width - 1] = 1;
+        }
+
+        // Scan down
+        let mut height_map = vec![vec![0; width]; height];
+        for i in 0..width {
+            height_map[0][i] = input[0][i];
+        }
+
+        for i in 1..height {
+            for j in 0..width {
+                if input[i][j] > height_map[i - 1][j] {
+                    height_map[i][j] = input[i][j];
+                    visible_map[i][j] = 1;
+                } else {
+                    height_map[i][j] = height_map[i - 1][j];
+                }
+            }
+        }
+
+        // Scan up
+        let mut height_map = vec![vec![0; width]; height];
+        for i in 0..width {
+            height_map[height - 1][i] = input[height - 1][i];
+        }
+
+        for pos_i in 1..height {
+            let i = height - (pos_i + 1);
+            for j in 0..width {
+                if input[i][j] > height_map[i + 1][j] {
+                    height_map[i][j] = input[i][j];
+                    visible_map[i][j] = 1;
+                } else {
+                    height_map[i][j] = height_map[i + 1][j];
+                }
+            }
+        }
+
+        // Scan right
+        let mut height_map = vec![vec![0; width]; height];
+        for i in 0..height {
+            height_map[i][0] = input[i][0];
+        }
+
+        for j in 1..width {
+            for i in 0..height {
+                if input[i][j] > height_map[i][j - 1] {
+                    height_map[i][j] = input[i][j];
+                    visible_map[i][j] = 1;
+                } else {
+                    height_map[i][j] = height_map[i][j - 1];
+                }
+            }
+        }
+
+        // Scan left
+        let mut height_map = vec![vec![0; width]; height];
+        for i in 0..height {
+            height_map[i][width - 1] = input[i][width - 1];
+        }
+
+        for pos_j in 1..width {
+            let j = width - (pos_j + 1);
+            for i in 0..height {
+                if input[i][j] > height_map[i][j + 1] {
+                    height_map[i][j] = input[i][j];
+                    visible_map[i][j] = 1;
+                } else {
+                    height_map[i][j] = height_map[i][j + 1];
+                }
+            }
+        }
+
+        let result = visible_map
+            .iter()
+            .map(|x| x.iter().sum::<i32>())
+            .sum::<i32>();
+
+        println!("P1 result {result}");
+        Ok(result)
     }
 
     fn part2(input: &mut Vec<Vec<i32>>, _part_1_solution: i32) -> Result<i32> {
