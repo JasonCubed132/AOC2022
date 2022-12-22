f26b0e63b8c561e320e5adb0ed4560622c48eef9
JasonCubed132 <amcb2002@googlemail.com>
2022-12-22 00:01:13 +0000

    Day 15 p2

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -34,14 +34,95 @@ impl Display for Point {
     }
 }
 
-impl SolutionLinear<(Vec<(Point, Point)>, isize), i32, i32> for Day15Solution {
-    fn load(input: &str) -> Result<(Vec<(Point, Point)>, isize)> {
+fn get_ranges_for_row(
+    data: &Vec<(Point, Point)>,
+    target_row: isize,
+) -> Result<Vec<(isize, isize)>> {
+    let mut distances = Vec::new();
+    let mut beacons = HashSet::new();
+
+    for (sensor, beacon) in data.clone() {
+        let distance = sensor.distance_to(&beacon);
+        distances.push((sensor, distance));
+        beacons.insert(beacon);
+    }
+
+    let mut covered_ranges = Vec::new();
+
+    for (sensor, distance) in distances.clone() {
+        let y_dist = (sensor.y - target_row).abs();
+        let x_dist = distance - y_dist;
+
+        if x_dist < 0 {
+            continue;
+        }
+
+        let left_x = sensor.x - x_dist;
+        let right_x = sensor.x + x_dist;
+        covered_ranges.push((left_x, right_x));
+    }
+
+    if covered_ranges.len() == 0 {
+        return Ok(Vec::new());
+    }
+
+    covered_ranges.sort_by(|(a1, a2), (b1, b2)| match a1.cmp(b1) {
+        Ordering::Equal => return a2.cmp(b2),
+        a => {
+            return a;
+        }
+    });
+
+    // println!("Original {:?}", covered_ranges);
+
+    let mut resolved_ranges = Vec::new();
+
+    let mut curr_range = covered_ranges.remove(0);
+    let mut pushed_modified_range;
+
+    loop {
+        let next_range = covered_ranges.remove(0);
+
+        if curr_range.1 + 1 >= next_range.0 {
+            pushed_modified_range = false;
+            if curr_range.1 < next_range.1 {
+                // Skip reassignment if can overlapping
+                curr_range = (curr_range.0, next_range.1);
+            }
+        } else {
+            resolved_ranges.push(curr_range);
+            curr_range = next_range;
+            pushed_modified_range = true;
+        }
+
+        if covered_ranges.len() == 0 {
+            break;
+        }
+    }
+
+    if !pushed_modified_range {
+        resolved_ranges.push(curr_range)
+    }
+
+    Ok(resolved_ranges)
+}
+
+impl SolutionLinear<(Vec<(Point, Point)>, isize, isize, isize, isize, isize), isize, isize>
+    for Day15Solution
+{
+    fn load(input: &str) -> Result<(Vec<(Point, Point)>, isize, isize, isize, isize, isize)> {
         let mut rows = input.lines().collect_vec();
-        let initial_row_re = Regex::new(r"target at y=(-?\d+)").unwrap();
+        let initial_row_re = Regex::new(
+            r"target at y=(-?\d+); min_x=(-?\d+); min_y=(-?\d+); max_x=(-?\d+); max_y=(-?\d+)",
+        )
+        .unwrap();
         let initial_row = rows.remove(0);
-        let target_row = initial_row_re.captures(initial_row).unwrap()[1]
-            .parse::<isize>()
-            .unwrap();
+        let initial_row_match = initial_row_re.captures(initial_row).unwrap();
+        let target_row = initial_row_match[1].parse::<isize>().unwrap();
+        let min_x = initial_row_match[2].parse::<isize>().unwrap();
+        let min_y = initial_row_match[3].parse::<isize>().unwrap();
+        let max_x = initial_row_match[4].parse::<isize>().unwrap();
+        let max_y = initial_row_match[5].parse::<isize>().unwrap();
 
         let re = Regex::new(
             r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
@@ -64,22 +145,19 @@ impl SolutionLinear<(Vec<(Point, Point)>, isize), i32, i32> for Day15Solution {
             output.push((sensor, beacon));
         }
 
-        Ok((output, target_row))
+        Ok((output, target_row, min_x, min_y, max_x, max_y))
     }
 
-    fn part1(input: &mut (Vec<(Point, Point)>, isize)) -> Result<i32> {
-        let (data, target_row) = input.clone();
+    fn part1(
+        input: &mut (Vec<(Point, Point)>, isize, isize, isize, isize, isize),
+    ) -> Result<isize> {
+        let (data, target_row, _, _, _, _) = input.clone();
 
-        let mut distances = Vec::new();
-        let mut beacons = HashSet::new();
+        let resolved_ranges = get_ranges_for_row(&data, target_row).unwrap();
 
         let mut cells_on_target_row_in_use = HashSet::new();
 
         for (sensor, beacon) in data.clone() {
-            let distance = sensor.distance_to(&beacon);
-            distances.push((sensor, distance));
-            beacons.insert(beacon);
-
             if sensor.y == target_row {
                 cells_on_target_row_in_use.insert(sensor.x);
             }
@@ -87,73 +165,6 @@ impl SolutionLinear<(Vec<(Point, Point)>, isize), i32, i32> for Day15Solution {
                 cells_on_target_row_in_use.insert(beacon.x);
             }
         }
-
-        // println!("{:?}", cells_on_target_row_in_use);
-
-        let mut covered_ranges = Vec::new();
-
-        for (sensor, distance) in distances.clone() {
-            let y_dist = (sensor.y - target_row).abs();
-            let x_dist = distance - y_dist;
-
-            if x_dist < 0 {
-                continue;
-            }
-
-            let left_x = sensor.x - x_dist;
-            let right_x = sensor.x + x_dist;
-            covered_ranges.push((left_x, right_x));
-            // println!(
-            //     "{} {} {} {} {} {}",
-            //     sensor, distance, y_dist, x_dist, left_x, right_x
-            // );
-        }
-
-        if covered_ranges.len() == 0 {
-            return Ok(0);
-        }
-
-        covered_ranges.sort_by(|(a1, a2), (b1, b2)| match a1.cmp(b1) {
-            Ordering::Equal => return a2.cmp(b2),
-            a => {
-                return a;
-            }
-        });
-
-        // println!("{:?}", covered_ranges);
-
-        let mut resolved_ranges = Vec::new();
-
-        let mut curr_range = covered_ranges.remove(0);
-        // println!("Curr range: {:?}", curr_range);
-        let mut pushed_modified_range;
-
-        loop {
-            let next_range = covered_ranges.remove(0);
-            // println!("Examining: {:?}", next_range);
-
-            if curr_range.1 + 1 >= next_range.0 {
-                pushed_modified_range = false;
-                if curr_range.1 < next_range.1 {
-                    // Skip reassignment if can overlapping
-                    curr_range = (curr_range.0, next_range.1);
-                }
-                // println!("Curr range: {:?}", curr_range);
-            } else {
-                resolved_ranges.push(curr_range);
-                pushed_modified_range = true;
-            }
-
-            if covered_ranges.len() == 0 {
-                break;
-            }
-        }
-
-        if !pushed_modified_range {
-            resolved_ranges.push(curr_range)
-        }
-
-        // println!("{:?}", resolved_ranges);
 
         let mut count = 0;
 
@@ -166,12 +177,33 @@ impl SolutionLinear<(Vec<(Point, Point)>, isize), i32, i32> for Day15Solution {
             }
         }
 
-        // println!("{}", count);
-        Ok(count.try_into().unwrap())
+        println!("{}", count);
+        Ok(count)
     }
 
-    fn part2(input: &mut (Vec<(Point, Point)>, isize), _part_1_solution: i32) -> Result<i32> {
-        todo!()
+    fn part2(
+        input: &mut (Vec<(Point, Point)>, isize, isize, isize, isize, isize),
+        _part_1_solution: isize,
+    ) -> Result<isize> {
+        let (data, _, min_x, min_y, max_x, max_y) = input.clone();
+
+        for i in min_y..max_y + 1 {
+            let ranges = get_ranges_for_row(&data, i).unwrap();
+            // println!("{:?}", ranges);
+            if ranges.len() == 1 {
+                continue;
+            }
+            if ranges.len() == 2 {
+                let y = i;
+                let x = ranges[0].1 + 1;
+                let result = x * 4000000 + y;
+                println!("x: {} y: {} result: {}", x, y, result);
+                return Ok(result.try_into().unwrap());
+            }
+            panic!();
+        }
+
+        Err(anyhow!("Did not arrive at a solution"))
     }
 }
 
@@ -183,7 +215,7 @@ mod tests {
 
     #[rstest]
     #[case(
-        "target at y=10
+        "target at y=10; min_x=0; min_y=0; max_x=20; max_y=20
 Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
@@ -199,9 +231,9 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3",
         26,
-        0
+        56000011
     )]
-    fn validate_linear(#[case] input: &str, #[case] expected_1: i32, #[case] expected_2: i32) {
+    fn validate_linear(#[case] input: &str, #[case] expected_1: isize, #[case] expected_2: isize) {
         let mut input = Day15Solution::load(input).unwrap();
         let p1 = Day15Solution::part1(&mut input).unwrap();
         let p2 = Day15Solution::part2(&mut input, p1).unwrap();
